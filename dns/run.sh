b88df13a94f4562712c3c0d99c74ca230c2b5843
Total Pusher <totalpusher@gmail.com>
2019-09-24 17:03:01 +0300

    dns: auto update root key for DNSSEC

--- dns/run.sh
+++ dns/run.sh
@@ -2,7 +2,9 @@
 
 envsubst < ./unbound.conf > /etc/unbound/unbound.conf
 
+# Print Warning from head
 head -n3 /etc/unbound/unbound.conf
+
 echo Run DNS on port: $PORT_DNS
 
 if [ "$DNS_DEBUG" == "on" ]; then
