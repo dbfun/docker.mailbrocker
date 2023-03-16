edca628eb4a3d85197e9fe076ec8487d253ae573
pushorigin <info@pushorigin.ru>
2023-03-16 06:13:33 +0300

    Удалил PORT_DNS

--- docker/dns/run.sh
+++ docker/dns/run.sh
@@ -5,7 +5,7 @@ envsubst < ./unbound.conf > /etc/unbound/unbound.conf
 # Print Warning from head
 head -n3 /etc/unbound/unbound.conf
 
-echo Run DNS on port: $PORT_DNS
+echo Run DNS on port: 53
 
 if [ "$DNS_DEBUG" == "on" ]; then
   # `-v` - verbose mode
