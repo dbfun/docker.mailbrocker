e03bbdf2965ecd7e77826ba9fbddde5720b06a4a
pushorigin <info@pushorigin.ru>
2023-03-17 01:43:54 +0300

    Добавил TODO

--- api/test/blacklist.js
+++ api/test/blacklist.js
@@ -8,6 +8,7 @@ const
   assert = require('assert'),
   _ = require('lodash'),
   // availableDNS = process.env.IP_DNS_RESOLVER ? process.env.IP_DNS_RESOLVER.trim().split(",").map(Function.prototype.call, String.prototype.trim) : [ "94.142.137.100", "94.142.136.100" ],
+  // TODO тут нужен IP адрес!
   availableDNS = [ "dns" ],
   blacklistDomains = require('../lib/Blacklist/dnsbl-domains'),
   testCases = [
