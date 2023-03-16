28851e0aace94acc98e14a382a039d9489ed234b
pushorigin <info@pushorigin.ru>
2023-03-16 09:16:41 +0300

    Поправил обновление named.root

--- docker/dns/run.sh
+++ docker/dns/run.sh
@@ -8,7 +8,11 @@ envsubst < ./unbound.conf > /etc/unbound/unbound.conf
 unbound-anchor -a /usr/share/dnssec-root/auto-trust.key
 chown unbound:unbound /usr/share/dnssec-root/auto-trust.key
 
-echo Run DNS on port: 53
+# Required for root hints
+# @see unbound.conf
+mkdir -p /usr/share/dns-root-hints/
+wget -q https://www.internic.net/domain/named.cache -O /usr/share/dns-root-hints/named.root
+chown -R unbound:unbound /usr/share/dns-root-hints/
 
 if [ "$DNS_DEBUG" == "on" ]; then
   # `-v` - verbose mode
